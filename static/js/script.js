61d7ff2ff65297b5f96ee18d0fa9547f369cf55d
Daniel Rios <drios@ooyala.com>
2014-03-18 19:56:11 -0600

    Adds pixel grid

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,52 @@
 /* Author: YOUR NAME HERE
 */
 
-$(document).ready(function() {   
+$(document).ready(function() {
+
+  var board_gui = {
+    create: function(x,y){
+
+      var height = 100.0/y;
+      var width = 100.0/x;
+
+      for(var j = 0; j < y; j++)
+        $(".board").append('<div class="row" row="' + j + '"></div>');
+
+      $(".row").each(function(){
+        $(this).css({ height: height + "%", width: "100%" });
+        for(var i = 0; i < x; i++){
+          $(this).append('<div class="col" col="' + i + '"></div>');
+        }
+      });
+
+      $(".col").each(function(){
+        $(this).css({ height: "100%", width: width + "%" });
+      });
+    }
+  }
+
+  board_gui.create(8,4);
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+  }
+
+  $('.col').bind('click', function(){
+    $(this).css("background-color", getRandomColor());
+  });
 
   var socket = io.connect();
 
   $('#sender').bind('click', function() {
-   socket.emit('message', 'Message Sent on ' + new Date());     
+   socket.emit('message', 'Message Sent on ' + new Date());
   });
 
   socket.on('server_message', function(data){
-   $('#receiver').append('<li>' + data + '</li>');  
+   $('#receiver').append('<li>' + data + '</li>');
   });
 });
